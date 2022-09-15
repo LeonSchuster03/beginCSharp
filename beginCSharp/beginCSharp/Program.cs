19b959bd532c0915c3bb1937c750b3655cdd93c4
Schuster, Leon <leon.schuster@tecalliance.net>
2022-09-15 11:40:23 +0200

    Aufgabe 1

---
+++ beginCSharp/beginCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1 = 3;
+            int zahl2 = 4;
+            int summe = zahl1 + zahl2;
+
+            Console.WriteLine("Die Summe der Zahlen ist " + summe);
+
+            Console.Read();
+        }
+    }
+}
