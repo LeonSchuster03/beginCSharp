1b19f897b9644d4fdbe4c7ac0ab19b8fd7dc66a7
Schuster, Leon <leon.schuster@tecalliance.net>
2022-09-15 11:46:04 +0200

    Aufgabe 2

--- beginCSharp/beginCSharp/Program.cs
+++ beginCSharp/beginCSharp/Program.cs
@@ -10,13 +10,17 @@ namespace beginCSharp
     {
         static void Main(string[] args)
         {
-            int zahl1 = 3;
-            int zahl2 = 4;
+            Console.WriteLine("Wähle eine Zahl");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wähle eine zweite Zahl");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+
             int summe = zahl1 + zahl2;
 
             Console.WriteLine("Die Summe der Zahlen ist " + summe);
 
-            Console.Read();
+            Console.ReadLine();
         }
     }
 }
