b624fff93f3845f25e0f4d39fa45f8512f35786f
Schuster, Leon <leon.schuster@tecalliance.net>
2022-09-16 14:37:13 +0200

    Übungsaufgaben

--- beginCSharp/beginCSharp/Program.cs
+++ beginCSharp/beginCSharp/Program.cs
@@ -13,15 +13,127 @@ namespace beginCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wir holen uns jetzt eine Zahl aus Number()");
-            int zahl = Number();
-            Console.WriteLine(zahl);
-            Console.ReadLine();
+            //Aufgabe 2
+            List<int> numbers = new List<int>() { 5, 3, 7};
+
+            //Aufgabe 3
+            var repeat = true;
+            do
+            {
+                Console.WriteLine("Bitte gib eine Zahl ein ");
+                numbers.Add(Convert.ToInt16(Console.ReadLine()));
+                Console.WriteLine("Willst du noch eine Zahl eingeben? (y/n)");
+                var userInput = Console.ReadLine();
+                if (userInput == "n")
+                {
+                    repeat = false;
+                }
+            } while (repeat);
+
+            //Aufgabe 4           
+            printList(numbers);
+
+            Console.ReadKey();
+            
+            //Aufgabe 5
+            numbers.Insert(2, 99);
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Aufgabe 6
+            numbers.AddRange(new int[]
+            {
+                91,
+                92,
+                93,
+            });
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(" ");
+
+            //Aufgabe 7
+            numbers.RemoveAt(1);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(" ");
+
+            //Aufgabe 8
+            numbers.Remove(99);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(" ");
+
+            //Aufgabe 9
+            numbers.RemoveAt(numbers.Count - 1);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(" ");
+
+            //Aufgabe 10
+            numbers.Sort();                 //sortieren und ausgeben
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(" ");
+
+
+            numbers.Reverse();              //reversen und ausgeben
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(" ");
+
+
+
+            //Aufgabe 11
+            foreach (var number in numbers)
+            {
+                if(number > 80)
+                {
+                    numbers.Remove(number);
+                }
+            }
+
+            
+
+
         }
 
-        static int Number()
+        //Methoden Aufgabe 1
+
+        static void printList(List<int> List)
         {
-            return 15;
+            foreach (var i in List)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("_________________");
         }
+
+
+
+
+        static double Div(double z1, double z2)
+        {
+            double quotient = z1 / z2;
+            return quotient;
+        }
+            //static  Rückgabetyp  MethodenName  (Parameter)
+
+
+        
+        
     }
 }
