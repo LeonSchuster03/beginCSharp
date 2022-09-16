79651086baba7e70282f7378170e3afa5b1b7878
Schuster, Leon <leon.schuster@tecalliance.net>
2022-09-16 11:13:43 +0200

    name lists

--- beginCSharp/beginCSharp/Program.cs
+++ beginCSharp/beginCSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,27 @@ namespace beginCSharp
         {
 
             List<string> names = new List<string>();
+            string repeat = "y";
+            int i = 1;
 
-            names.Add("Leon");
-            names.Add("Erwin");
-            names.Add(Console.ReadLine());
+            do
+            {
+                Console.WriteLine($"Gebe den {i}. Namen ein, den du hinzufügen willst");
+                i++;
+                names.Add(Console.ReadLine());
+                Console.WriteLine("Willst du einen weiteren Namen einfügen ( y / n )?");
+                repeat = Console.ReadLine();
+            } while (repeat == "y");
 
-            Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine("Die Liste lautet:");
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadKey();
         }
     }
 }
